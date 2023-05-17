222f770429f705ca5d981aeae708065719186ad9
krunalsukhwani <krunal.sukhwani@gmail.com>
2023-05-17 19:52:05 -0400

    let and const

--- js/main.js
+++ js/main.js
@@ -5,3 +5,10 @@ console.log(squareValue(11));
 console.log(halfOf(10));
 doSomething();
 console.log(yourName);
+
+//let - It will allow you to change the value after initialization.
+//const - It won't allow you to change the value after initialization.
+let myName = "krunal";
+console.log(myName);
+myName = "Leonardo";
+console.log(myName);
