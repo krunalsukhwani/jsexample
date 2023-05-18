c322fc577e88d4c75b514c9bd30c210bdf753717
krunalsukhwani <krunal.sukhwani@gmail.com>
2023-05-17 20:03:55 -0400

    rest example

---
+++ js/restExample.js
@@ -0,0 +1,7 @@
+function myStudents(professor,...students){
+    console.log("Professor Name: " + professor);
+    console.log("Number of students: " + students.length);
+    console.log(students[2]);
+}
+
+myStudents("Krunal","jorge","Ishan","Laura");
